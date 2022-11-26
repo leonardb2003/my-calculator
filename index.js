7041c24138e72a2432dc61f409e180883844fee5
leonardb2003 <b.leonardb2003@gmail.com>
2022-11-26 13:27:07 +0200

    first

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const output = document.getElementById("output");
+let deci = true;
 
 function display(arg){
-    output.value += arg;
+    output.value = output.value + arg;
+}
+function displayOpp(arg){
+    output.value = output.value + arg;
+    deci = true;
+}
+function dot(arg) {
+    if(deci === true){
+        output.value = output.value + arg;
+        deci = false;
+    }
 }
 function allClear(){
     output.value = '';
+    deci = true;
 }
 function del(){
     output.value = output.value.slice(0,-1);
+
 }
 function calculate(){
     output.value = eval(output.value);
+    deci = true;
+    
 }
 
 
